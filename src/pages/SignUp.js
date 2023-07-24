c1c2e9d74629031e4679bdcdadb26b4be0113b99
Ayosnowchild <adekoyaasap@gmail.com>
2023-07-24 12:42:39 +0100

    context lesson

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,37 @@
-import React from "react";
-
+import React, { useContext, useState } from "react";
+import { NameContext } from "../App";
+import "./SignUp.css";
+import Nav from "../components/Navbar/nav";
+import { useNavigate } from "react-router-dom";
 function SignUp() {
-  return <div>SignUp</div>;
+  const router = useNavigate();
+  const { setName } = useContext(NameContext);
+  const [input, setInput] = useState("");
+  function handleChange(e) {
+    setInput(e.target.value);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    setName(input);
+    router("/");
+  }
+  return (
+    <div>
+      <Nav />
+      <form onSubmit={handleSubmit}>
+        <div className="form-sec">
+          <label>input a username:</label>
+          <input
+            name="name"
+            value={input}
+            placeholder="Username"
+            onChange={handleChange}
+          />
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
